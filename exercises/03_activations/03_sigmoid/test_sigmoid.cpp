e969deb8c34e58711cb3c2e4140ee0d5c9beaf73
NickEdwards7502 <nickedwards7502@gmail.com>
2024-11-10 06:38:51 +0000

    Implement sigmoid activation

---
+++ exercises/03_activations/03_sigmoid/test_sigmoid.cpp
@@ -0,0 +1,24 @@
+#include "utils/utils_stdout.h"
+#include "sigmoid.h"
+#include <vector>
+
+int main()
+{
+    Sigmoid activation = Sigmoid();
+
+    std::vector<double> inputs1 = {0, 2, -1, 0.5, -2.7, 1.1, 2.2, -100};
+
+    nn_utils::print(activation.forward(inputs1));
+
+    std::vector<std::vector<double>> inputs2 = {
+        {1.0, 0.5, 3.0, 2.5},
+        {0.0, 5.0, -1.0, 2.0},
+        {-1.5, 2.7, 3.3, -0.8},
+    };
+
+    nn_utils::print(inputs2);
+
+    nn_utils::print(activation.forward(inputs2));
+
+    return 0;
+}
